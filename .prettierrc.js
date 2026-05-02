92250cc75e947c6917a50f96db060b0814a3a6b2
ivanbyone <ivanbyone@gmail.com>
2026-05-02 11:27:19 +0300

    chore: setup npm and prepare prettier

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+// Config file for Prettier
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: true,
+};
+
+export default config;
